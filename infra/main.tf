4980ca8c8bc045fbde20e23ac7f23b1ebe668de4
David Pazdera <david.pazdera@outlook.com>
2025-04-15 19:27:35 +0200

    Update main.tf
    Declare remote state using Azure Blob.

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,15 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terrateam-demo-state-rg" # Replace with your resource group name
+    storage_account_name = "terrateamteststate"      # Replace with your storage account name
+    container_name       = "terrateam-state"     # Replace with your container name
+    key                  = "terraform.tfstate"       # Path to the state file in the container
+  }
+}
+
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.resource_group_location
